6213142cf43687a9fe706ebc6a9998a13dc80062
Paul Bowsher <paul.bowsher@gmail.com>
2020-04-04 23:43:34 +0100

    Add Report A Problem API

--- Makefile
+++ Makefile
@@ -16,6 +16,7 @@ deploy: package.zip
 	aws s3 cp package.zip s3://theyhelpyou/package-$(word 1,$(shell md5sum package.zip)).zip
 	aws lambda update-function-code --region eu-west-2 --function-name theyhelpyou_fetch_by_postcode --s3-bucket theyhelpyou --s3-key package-$(word 1,$(shell md5sum package.zip)).zip --publish
 	aws lambda update-function-code --region eu-west-2 --function-name theyhelpyou_import_sheet --s3-bucket theyhelpyou --s3-key package-$(word 1,$(shell md5sum package.zip)).zip --publish
+	aws lambda update-function-code --region eu-west-2 --function-name theyhelpyou_report_a_problem --s3-bucket theyhelpyou --s3-key package-$(word 1,$(shell md5sum package.zip)).zip --publish
 
 s3: site/index.html
 	aws s3 sync site s3://theyhelpyou/ --acl public-read --cache-control max-age=10
---
+++ api/report_a_problem.py
@@ -0,0 +1,98 @@
+import base64
+import json
+import os
+
+from helpers import format_response
+from models import CommunityHub
+
+from slack_webhook import Slack
+from pynamodb.models import DoesNotExist
+
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+# xray_recorder.configure()
+# patch_all()
+
+slack = Slack(url=os.environ.get("WEBHOOK_URL"))
+
+def lambda_handler(event, context):
+    print(event)
+    if "body" not in event:
+        return format_response("Body not supplied", 400)
+
+    try:
+        if event["isBase64Encoded"]:
+            data = json.loads(base64.b64decode(event["body"]))
+        else:
+            data = json.loads(event["body"])
+    except:
+        raise
+        return format_response("Body must be JSON", 400)
+
+    if "gss" not in data:
+        return format_response("GSS code must be supplied", 400)
+
+    try:
+        hub = CommunityHub.get(data["gss"])
+    except DoesNotExist:
+        return format_response("GSS code not found", 404)
+
+    fields = [
+            {
+                "title": "Name",
+                "value": hub.name,
+                "short": True,
+            },
+            {
+                "title": "Homepage URL",
+                "value": hub.homepage_url,
+                "short": True,
+            },
+            {
+                "title": "Phone Number",
+                "value": hub.phone,
+                "short": True,
+            },
+            {
+                "title": "Date Collected",
+                "value": hub.date_collected,
+                "short": True,
+            },
+            {
+                "title": "Hub URL",
+                "value": hub.hub_url,
+                "short": False,
+            },
+            {
+                "title": "Email",
+                "value": hub.email,
+                "short": True,
+            },
+            {
+                "title": "Notes",
+                "value": hub.notes,
+                "short": False,
+            }
+        ]
+
+    if "message" in data:
+        fields.append({
+            "title": "Message from user",
+            "value": data["message"],
+            "short": False,
+        })
+
+    slack.post(
+        text="Problem reported with the data for {}".format(hub.name),
+        attachments=[{
+            "fallback": "Problem reported with the data for {}".format(hub.name),
+            "text": "*Current data:*",
+            "fields": fields,
+        }],
+    )
+
+
+
+if __name__ == "__main__":
+    lambda_handler({"body": json.dumps({"gss": "E10000030", "message": "Test from Paul"}), "isBase64Encoded": False}, {})
