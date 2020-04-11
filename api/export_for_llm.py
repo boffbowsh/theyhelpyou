c9cc72d2ed80c78a56612968d9fb0b69b5bb9e2a
Paul Bowsher <paul.bowsher@gmail.com>
2020-04-11 13:29:17 +0100

    Exclude main Northern Ireland region from export

--- api/export_for_llm.py
+++ api/export_for_llm.py
@@ -10,23 +10,28 @@ def lambda_handler(event, context):
     if event["queryStringParameters"]["type"] not in ["shielding", "vulnerable", "volunteering"]:
         return format_response({"error": "type must be one of 'shielding', 'vulnerable', or 'volunteering'"}, 400)
 
-    filters = []
+    t = event["queryStringParameters"]["type"]
+
+    # Exclude our Northern Ireland hack
+    filters = CommunityHub.gss != "N07000001"
+
     if "nation" in event["queryStringParameters"]:
         if event["queryStringParameters"]["nation"] not in ["E", "N", "S", "W"]:
             return format_response({"error": "nation must be one of 'E', 'N', 'S' or 'W'"}, 400)
         else:
-            filters.append(CommunityHub.gss.startswith(event["queryStringParameters"]["nation"]))
+            filters = filters & CommunityHub.gss.startswith(event["queryStringParameters"]["nation"])
 
     results = {}
-    for item in CommunityHub.scan(*filters):
-        results[item.gss] = item.hub_url
+    for item in CommunityHub.scan(filters):
+        if t + "_url" in item.attribute_values:
+            results[item.gss] = item.attribute_values[t + "_url"]
 
     return format_response(results, 200)
 
 if __name__ == "__main__":
     print(lambda_handler(
         {"queryStringParameters": {
-            "type": "shielding",
+            "type": "volunteering",
             "nation": "E",
         }},
         {}
